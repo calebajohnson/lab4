aee75c8215474b89d7ec92a8f4768636c944f2ac
Caleb <calebjohnson@ufl.edu>
2017-09-26 17:46:03 -0400

    asks for an integer, will display "good" whether it's an integer or not

---
+++ src/lab4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class lab4
+{
+    public static void main(String args[])
+    {
+        int num;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+
+        num = input.nextInt();
+
+        System.out.print("Good");
+
+        return;
+    }
+}
